bf382cfb34f83f78aaf20d336f028545f0e27982
rohgoon <rohgoon@gmail.com>
2016-12-13 14:54:50 +0900

    한줄 추가하여 충돌 의도적 발생시킬것

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package kr.or.dgit.bigdata.helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("의도적으로 충돌");
 	}
 }
