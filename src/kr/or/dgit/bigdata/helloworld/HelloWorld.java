d10f694ff2d5cfd51bb168ff929d60cbc07b20b0
rohgoon <rohgoon@gmail.com>
2016-12-13 14:53:57 +0900

    master에서 추가

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -3,5 +3,7 @@ package kr.or.dgit.bigdata.helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("hotfix에서 작업");
+		System.out.println("master에서 추가");
 	}
 }
