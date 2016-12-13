f04521a8fcf42a8181f2c5419606439f4d26288f
rohgoon <rohgoon@gmail.com>
2016-12-13 14:55:27 +0900

    핫픽스에서 추가

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -3,6 +3,7 @@ package kr.or.dgit.bigdata.helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("hotfix에서 추가");
 		System.out.println("의도적으로 충돌");
 	}
 }
